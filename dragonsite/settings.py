45c30cf2a394d4ccf0189bad23c1c48669e38f74
Dofamineuser <Ahtoxa19971312@gmail.com>
2022-05-26 12:57:12 +0300

    add app managefiles

--- dragonsite/settings.py
+++ dragonsite/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'imagekit',
-    'blog'
+    'blog',
+    'managefiles'
 
 ]
 
--- dragonsite/urls.py
+++ dragonsite/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('downloads/', include('managefiles.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
