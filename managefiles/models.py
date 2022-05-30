a74b9edd33c02fdb04aecf3a6a914f7af4b71faa
Dofamineuser <Ahtoxa19971312@gmail.com>
2022-05-30 12:20:05 +0300

    add app managefiles

--- managefiles/models.py
+++ managefiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from home.models import Team
 
-# Create your models here.
+
+class File(models.Model):
+    title = models.CharField("Назва", max_length=30)
+    slug = models.SlugField()
+    description = models.CharField("Короткий опис", max_length=150)
+    text = models.TextField("Повний опис файлу")
+    file = models.FileField(upload_to='downloads')
+    organizer = models.ForeignKey(Team, on_delete=models.CASCADE)
--- managefiles/views.py
+++ managefiles/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import File
+from home.models import OwnerInfo
+from blog.models import BlogCat
+
+file = File.objects.all()
+owner = OwnerInfo.objects.first()
+categories = BlogCat.objects.all()
 
 
 def manage_files(request):
-    return render(request, 'managefiles/managefiles.html')
+    return render(request, 'managefiles/managefiles.html', {
+        'file': file,
+        'owner': owner,
+        'categories': categories
+    })
